46e0a6e7ad430f6ce1db77fdad859c595eb06ad4
Rija Ménagé <git@rijam.sent.as>
2015-04-03 12:54:46 +0800

    when generating wp-config.php, use getenv to read value of linked mysql details so that literal value don't become hardcoded in the commited docker image, but dynamically read during runtime

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
 	# configuring wp-config with DB connection details from linked container then generate random password for keys
-  sed -e "s/database_name_here/$DB_ENV_MYSQL_DATABASE/
-  s/localhost/$DB_PORT_3306_TCP_ADDR/
-  s/username_here/$DB_ENV_MYSQL_USER/
-  s/password_here/$DB_ENV_MYSQL_PASSWORD/
+  sed -e "s/'database_name_here'/getenv('DB_ENV_MYSQL_DATABASE')/
+  s/'localhost'/getenv('DB_PORT_3306_TCP_ADDR')/
+  s/'username_here'/getenv('DB_ENV_MYSQL_USER')/
+  s/'password_here'/getenv('DB_ENV_MYSQL_PASSWORD')/
   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
